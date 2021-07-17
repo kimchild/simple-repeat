104770fb3bc2a3e6e37f7061a8ba9f9531bb6310
kimchild <allcreation@nate.com>
2021-07-17 23:52:11 +0900

    feat: 변경화면 기능 추가
    코틀린에서 정규식 Replace
    \U기능을 찾아야 카멜 케이스 변경이 됨

--- src/main/kotlin/com/simple/repeat/home/HomeController.kt
+++ src/main/kotlin/com/simple/repeat/home/HomeController.kt
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class HomeController {
@@ -13,4 +15,15 @@ class HomeController {
         model["title"] = "Home"
         return "Home"
     }
+
+    @PostMapping("/replace")
+    fun replace(model: Model,
+                @RequestParam("camelText") camelText:String): String {
+
+        val result = camelText.replace("(_)(\\w{1})".toRegex(), "\\U$2")
+        model["title"] = "Replace"
+        model["text"] = result
+        return "Replace"
+    }
+
 }
