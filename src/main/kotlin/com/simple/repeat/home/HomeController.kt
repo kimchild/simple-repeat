5023c2e4f27dd65d0f886b6a068cf10ac4ddfeb5
kimchild <allcreation@nate.com>
2021-07-17 23:05:21 +0900

    feat: 첫 메인화면 기능 추가
    자동화 도움 프로젝트 개발을 위한
    첫 웹화면 프로젝트 기능 추가

---
+++ src/main/kotlin/com/simple/repeat/home/HomeController.kt
@@ -0,0 +1,16 @@
+package com.simple.repeat.home
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    fun home(model: Model): String {
+        model["title"] = "Home"
+        return "Home"
+    }
+}
